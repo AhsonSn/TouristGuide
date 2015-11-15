0775dbd9b332891d0161dcace97fcd6b51e68ba8
Zoltan Dalmadi <dmz985@gmail.com>
2015-11-15 23:34:14 +0100

    Basic database integration

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
 from app.basic.views import basic
 from app.users.views import users
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
 app.config.from_object('config')
+
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+
 app.register_blueprint(basic)
 app.register_blueprint(users)
