9c5b3929e6052be58035bd956d204660e207729c
Zoltan Dalmadi <dmz985@gmail.com>
2015-11-25 23:34:01 +0100

    File upload is working!

--- app/basic/models.py
+++ app/basic/models.py
@@ -1,12 +1,21 @@
-from werkzeug.utils import secure_filename
+import random
+import string
+import os
+from flask import current_app
 
 
 class UploadManager(object):
     @staticmethod
     def upload_avatar(image):
-        filename = secure_filename(image.filename)
+        filename = generate_random_string(20) + os.path.splitext(image.filename)[1]
+        image.save(os.path.join(current_app.config['AVATAR_UPLOAD_FOLDER'], filename))
         print("Image uploaded: {}".format(filename))
         return filename
+
+
+def generate_random_string(length):
+    return "".join(random.choice(string.ascii_letters + string.digits)
+                   for _ in range(length))
 
 
 sidebar_items = [
