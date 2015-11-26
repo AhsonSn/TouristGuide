a3129b7c535b1e2e4d629a1362c8fed8a10137c3
Orban Istvan <firstvan@live.com>
2015-11-26 13:39:08 +0100

    Test update password method.

--- tests/testdatabase.py
+++ tests/testdatabase.py
@@ -4,6 +4,7 @@ import unittest
 from app import create_app
 from app.db.dbfactory import DBFactory
 from app.db.models import Experience
+from werkzeug.security import check_password_hash
 
 
 class DBManagerTest(unittest.TestCase):
@@ -28,6 +29,11 @@ class DBManagerTest(unittest.TestCase):
     def test_insert_tour(self):
         #self.instance.Tour.insert_tour("Bükki túra2", "2015-11-19 10:10", "2015-11-20 10:10", "", 1, 1, "Káprázatos túrahely.")
         self.assertEquals(True, True)
+
+    def test_update_pwd(self):
+        print(self.instance.User.update_pwd("test", "test1", "test1"))
+        usr = self.instance.User.get_user_with_name("test")
+        self.assertEquals(True, check_password_hash(usr.password, "test1"))
 
 if __name__ == '__main__':
     unittest.main()
