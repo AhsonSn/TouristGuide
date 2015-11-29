8714d218ae5238dc8b86a05f0707419e52b0ed41
Zoltan Dalmadi <dmz985@gmail.com>
2015-11-29 02:27:26 +0100

    Fixed redirection to tours default page

--- app/users/views.py
+++ app/users/views.py
@@ -25,7 +25,7 @@ def login():
 
         login_user(user, remember_me)
 
-        return redirect(url_for('tours.tours'))
+        return redirect(url_for('tours.tours_default'))
 
     return render_template('login.html', login_form=login_form,
                            sidebar_items=sidebar_items, error=False)
