1e2b4ec25c5e8d8e0c9ec622ab26340634d3d2c2
Ferenc Sipos <siposferenc93@gmail.com>
2015-12-05 16:26:57 +0100

    + Refactoring weather module: sorting weathers by date, MM.DD format of the date.

--- app/weather/weather.py
+++ app/weather/weather.py
@@ -1,12 +1,12 @@
 
 class Weather(object):
     def __init__(self, d):
-        self.morn_temp = d["temp"]["morn"]
-        self.day_temp = d["temp"]["day"]
-        self.eve_temp = d["temp"]["eve"]
-        self.night_temp = d["temp"]["night"]
-        self.min_temp = d["temp"]["min"]
-        self.max_temp = d["temp"]["max"]
+        self.morn_temp = int(d["temp"]["morn"])
+        self.day_temp = int(d["temp"]["day"])
+        self.eve_temp = int(d["temp"]["eve"])
+        self.night_temp = int(d["temp"]["night"])
+        self.min_temp = int(d["temp"]["min"])
+        self.max_temp = int(d["temp"]["max"])
         self.humidity = d["humidity"]
         self.main_weather = d["weather"][0]["main"]
         self.pressure = d["pressure"]
